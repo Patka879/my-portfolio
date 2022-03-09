fefa95383ec4273673f5542ea16c81f622f56e6e
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-03-09 22:19:14 +0100

    skills section updated, map for skills created

--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -1,28 +1,18 @@
+import data from "../skillsData"
+
 export default function Skills() {
+
     return (
         <div className="skills-container">
-            <ul className="skills-list">
-                <li>HTML</li>
-                <li> CSS</li>
-                <li>JavaScript</li>
-                <li>React</li>
-                <li>node.js</li>
-                <li>git</li>
-                <li>Type Script</li>
-                <li>Jest</li>
-                <li>SAP</li>
-                <li>ERP</li>
-                <li>JIRA</li>
-            </ul>
-            <ul className="skills-list">
-                <li>English C1</li>
-                <li>Responsive Design</li>
-                <li>Document object model</li>
-                <li>Teamwork</li>
-                <li>Communication Skills</li>
-                <li>Problem Solving</li>
-                
-            </ul>
+            {data.map(item => {
+                return (  
+                    <span className="skills-list" key={item.id}>
+                        <img className="skill--icon" alt="skill icon"src={item.coverImg} />
+                        <p className="skill--title">{item.title}
+                        </p>
+                    </span>    
+                )
+            })}
         </div>
     )
 }
---
+++ portfolio/src/skillsData.js
@@ -0,0 +1,101 @@
+import html from "./images/html.svg"
+import css from "./images/css.svg"
+import javascriptpic from "./images/javascriptpic.svg"
+import reactpic from "./images/react.svg"
+import nodepic from "./images/node.svg"
+import typescriptpic from "./images/typescript.svg"
+import gitpic from "./images/git.svg"
+import english from "./images/english.svg"
+import communication from "./images/communication.svg"
+import team from "./images/team.svg"
+import erp from "./images/ERP.svg"
+import sap from "./images/sap.svg"
+import responsive from "./images/responsive.svg"
+import jira from "./images/jira.svg"
+import jsonpic from "./images/json.svg"
+import timemng from "./images/timemng.svg"
+
+
+export default [
+    {
+        id: 1,
+        title: "HTML",
+        coverImg: html,
+    },
+    {
+        id: 2,
+        title: "CSS",
+        coverImg: css
+    },
+    {
+        id: 3,
+        title: "JavaScript",
+        coverImg: javascriptpic,
+    },
+    {
+        id: 4,
+        title: "React",
+        coverImg: reactpic,
+    },
+    {
+        id: 5,
+        title: "node.js",
+        coverImg: nodepic,
+    },
+    {
+        id: 6,
+        title: "Type Script",
+        coverImg: typescriptpic,
+    },
+    {
+        id: 7,
+        title: "Git",
+        coverImg: gitpic,
+    },
+    {
+        id: 8,
+        title: "JSON",
+        coverImg: jsonpic,
+    },
+    {
+        id: 9,
+        title: "JIRA",
+        coverImg: jira,
+    },
+    {
+        id: 10,
+        title: "ERP Optima",
+        coverImg: erp,
+    },
+    {
+        id: 11,
+        title: "SAP",
+        coverImg: sap,
+    },
+    {
+        id: 12,
+        title: "English C1",
+        coverImg: english,
+    },
+    {
+        id: 13,
+        title: "Communication Skills",
+        coverImg: communication,
+    },
+    {
+        id: 14,
+        title: "Teamwork",
+        coverImg: team,
+    },
+    {
+        id: 15,
+        title: "Responsive web design",
+        coverImg: responsive,
+    },
+    {
+        id: 16,
+        title: "Time Management",
+        coverImg: timemng,
+    },
+
+]
