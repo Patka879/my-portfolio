d953c4a0a0b33fa22b2aaf5f6c17ebc554ce53f4
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-03-09 20:04:27 +0100

    skills list edited, container updated

--- portfolio/src/Components/Skills.js
+++ portfolio/src/Components/Skills.js
@@ -1,10 +1,28 @@
-import wordcloud from "../images/wordcloud.png"
-
-
 export default function Skills() {
     return (
-        <div>
-            <img src={wordcloud} alt="JavaScript React HTML CSS node.js" className="skills-cloud" />
+        <div className="skills-container">
+            <ul className="skills-list">
+                <li>HTML</li>
+                <li> CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>node.js</li>
+                <li>git</li>
+                <li>Type Script</li>
+                <li>Jest</li>
+                <li>SAP</li>
+                <li>ERP</li>
+                <li>JIRA</li>
+            </ul>
+            <ul className="skills-list">
+                <li>English C1</li>
+                <li>Responsive Design</li>
+                <li>Document object model</li>
+                <li>Teamwork</li>
+                <li>Communication Skills</li>
+                <li>Problem Solving</li>
+                
+            </ul>
         </div>
     )
 }
