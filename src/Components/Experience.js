d05b4ff136c206aaea170aa242b49b1368126164
Mysiak <Patrycja.Mysiak@PerkinElmer.com>
2022-06-01 16:16:44 +0200

    docker-compose corrected to unit-converter

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,12 +2,12 @@ export default function Experience() {
     return (
     <div className="experience-container">
         <ul>Freelance Developer
-            <li>Building websites from scratch, using semantic elements and "mobile first" approach. planning their structure and design</li>
+            <li>Building websites from scratch, using semantic elements and "mobile first" approach. Planning their structure and design</li>
             <li>Creating react components, dumb components and using hooks</li>
             <li>Troubleshooting problems with JavaScript logic and refining perfect styles</li>
-            <li>Writing unit tests (JEST) </li>
+            <li>Writing unit tests (JEST)</li>
             <li>Creating animations using css animations and keyframes</li>
-            <li>Configuring VPS servers and dockerized appswith Docker-Compose</li>
+            <li>Configuring VPS servers and dockerized apps with Docker-Compose</li>
         </ul>
     </div>
     )
