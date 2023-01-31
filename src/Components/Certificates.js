3d74de9fa5ad5249531ce378e0d80af1bac67be0
Patka879 <mysiak.patrycja@gmail.com>
2023-01-31 21:45:14 +0100

    styles changed

--- src/Components/Certificates.js
+++ src/Components/Certificates.js
@@ -2,15 +2,14 @@ import data from '../certData'
 
 export default function Certificates() {
     return (
-        <div className='certData'>
+        <div className='cert-data'>
+            <p className='certificates-title'>Certificates</p>
             {data.map(item => {
                 return (
-                    <div className='certList'>
-                        <a className='certificate' target='_blank' rel='noreferrer' href={item.link} key={item.id}>{item.title}
-                            <span className="cert--title">{item.title}</span>
-                            <span className="cert-description">{item.description}</span>
-                        </a>
-                    </div>
+                    <a className='certificate' target='_blank' rel='noreferrer' href={item.link} key={item.id}>
+                        <p className="cert--title">{item.title}</p>
+                        <p className="cert--description">{item.description}</p>
+                    </a>
                 )
             })}
         </div>
