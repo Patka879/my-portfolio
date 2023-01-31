1bb245b3c970da4e1493dedc5b619a01383449e9
Patka879 <mysiak.patrycja@gmail.com>
2023-01-31 20:59:21 +0100

    Folder holding certificates data created

---
+++ src/certData.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        title: 'JavaScript Algorithms & Data Structures',
+        description: `Estimated 300 hours of coursework with it's primary focus being JavaScript and
+        Data Structures. As well as OOP, Functional Programming and more.`
+    },
+    {
+        id: 2,
+        title: 'Responsive Web Design',
+        description: `Estimated 300 hours of course work that Includes HTML5, CSS3, FlexBox and
+        CSS Grid that covers 100s of challenges and 5 projets`
+    },
+    {
+        id: 3,
+        title: 'The Frontend Developer Career Path',
+        description: `Over 70 hours of course work, coding challenges, and real-world projects
+        containing HTML, CSS, JavaScript, React, UI design and more.`
+    }
+]
