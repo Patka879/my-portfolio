cf565df13bd179e0da369af402edda035dee2c89
Patka879 <mysiak.patrycja@gmail.com>
2023-02-04 00:02:56 +0100

    media-queries for welcome and project section changed

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,5 +1,4 @@
 import Projects from "./Projects"
-import {Routes, Route, Navigate,} from "react-router-dom"
 import { useEffect } from "react"
 import { useLocation } from "react-router"
 import Experience from "./Experience"
@@ -17,19 +16,15 @@ export default function Content() {
     )
 
     return (
-        // <Routes>
-        //     <Route path="/" element={<Navigate to="projects" />}></Route>
-        //     <Route path="experience" element={<Experience />}></Route>
-        //     <Route path="skills" element={<Skills />}></Route>
-        //     <Route path="projects" element={<Projects />}></Route>
-        //     <Route path="*" element={<Navigate to="/projects" replace />} />
-        // </Routes> 
         <div>
-            <h1 className='container-title' id="projects">Projects</h1>
+            <div className='anchor-div' id="projects"></div>
+            <h1 className='container-title'>Projects</h1>
             <Projects />
-            <h1 className='container-title' id="experience">Experience</h1>
+            <div className='anchor-div' id="experience"></div>
+            <h1 className='container-title'>Experience</h1>
             <Experience />
-            <h1 className='container-title' id="skills">Skills</h1>
+            <div className='anchor-div' id="skills"></div>
+            <h1 className="container-title">Skills</h1>
             <Skills />
         </div>
     )
