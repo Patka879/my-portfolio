ab048a418761c251dbe3b77364ce24468c31196d
Patka879 <mysiak.patrycja@gmail.com>
2023-02-03 14:45:12 +0100

    background of welcome-message changed

--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -2,7 +2,7 @@ export default function WelcomePage() {
     return(
         <div className='welcome-container'>
             <div className='welcome-message'>
-                <h1 className='welcome-header'>Hello, I am <span>Patrycja</span></h1>
+                <h1 className='welcome-header'>Hello, I am <span className='name'>Patrycja</span></h1>
                 <h1 className='welcome-header'>I'am a Frontend Developer.</h1>
                 <a href='#anchor'>
                     <button className='welcome-btn'>See my work </button>
