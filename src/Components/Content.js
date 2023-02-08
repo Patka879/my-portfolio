1a12e8caedc2ff1a94b6b6853935ca3c7e51fd30
Patka879 <mysiak.patrycja@gmail.com>
2023-02-08 22:45:15 +0100

    fade in effect added on sections

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import { useLocation } from "react-router"
 import Experience from "./Experience"
 import Skills from "./Skills"
+import { Fade } from "react-reveal"
 
 
 export default function Content() {
@@ -18,14 +19,20 @@ export default function Content() {
     return (
         <div>
             <div className='anchor-div' id="projects"></div>
+            <Fade left>
             <h1 className='container-title'>Projects</h1>
             <Projects />
+            </Fade>
             <div className='anchor-div' id="experience"></div>
+            <Fade right>
             <h1 className='container-title'>Experience</h1>
             <Experience />
+            </Fade>
             <div className='anchor-div' id="skills"></div>
+            <Fade left>
             <h1 className="container-title">Skills</h1>
             <Skills />
+            </Fade>
         </div>
     )
 }
