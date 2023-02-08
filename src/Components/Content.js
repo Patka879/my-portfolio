8cc96bf47aadf59402ec7b162eb9c18173efbf49
Patka879 <mysiak.patrycja@gmail.com>
2023-02-08 23:46:49 +0100

    reveal added

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router'
 import Experience from './Experience'
 import Skills from './Skills'
+import { Slide } from 'react-awesome-reveal'
 
 
 export default function Content() {
@@ -18,14 +19,20 @@ export default function Content() {
     return (
         <div>
             <div className='anchor-div' id='projects'></div>
+            <Slide duration={2000}>
             <h1 className='container-title'>Projects</h1>
             <Projects />
+            </Slide>
             <div className='anchor-div' id='experience'></div>
+            <Slide duration={2000}>
             <h1 className='container-title'>Experience</h1>
             <Experience />
+            </Slide>
             <div className='anchor-div' id='skills'></div>
+            <Slide duration={2000}>
             <h1 className='container-title'>Skills</h1>
             <Skills />
+            </Slide>
         </div>
     )
 }
