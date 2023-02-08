ae13a4a2eeedc20d17a1bfef36ffb994679d650d
Patka879 <mysiak.patrycja@gmail.com>
2023-02-08 22:54:20 +0100

    code cleaned

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,16 +1,16 @@
-import Projects from "./Projects"
-import { useEffect } from "react"
-import { useLocation } from "react-router"
-import Experience from "./Experience"
-import Skills from "./Skills"
-import { Fade } from "react-reveal"
+import Projects from './Projects'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router'
+import Experience from './Experience'
+import Skills from './Skills'
+import { Fade } from 'react-reveal'
 
 
 export default function Content() {
     const location = useLocation()   
     useEffect(
         function() {
-            console.log("changed location", window.scrollY)
+            console.log('changed location', window.scrollY)
             window.scrollTo(0, 0)
         }, 
         [location]
@@ -18,19 +18,19 @@ export default function Content() {
 
     return (
         <div>
-            <div className='anchor-div' id="projects"></div>
+            <div className='anchor-div' id='projects'></div>
             <Fade left>
             <h1 className='container-title'>Projects</h1>
             <Projects />
             </Fade>
-            <div className='anchor-div' id="experience"></div>
+            <div className='anchor-div' id='experience'></div>
             <Fade right>
             <h1 className='container-title'>Experience</h1>
             <Experience />
             </Fade>
-            <div className='anchor-div' id="skills"></div>
+            <div className='anchor-div' id='skills'></div>
             <Fade left>
-            <h1 className="container-title">Skills</h1>
+            <h1 className='container-title'>Skills</h1>
             <Skills />
             </Fade>
         </div>
